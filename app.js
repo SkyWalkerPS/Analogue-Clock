4a9e7f3fb7bdc97e007b1f61e84f22867e185799
Prakhar Saxena <iamprakhars@gmail.com>
2021-07-25 14:47:38 +0530

    first commit to the file

---
+++ app.js
@@ -0,0 +1,16 @@
+setInterval(()=>{
+    var d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+
+    
+    hrotation = htime * 30 + mtime/2;
+    mrotation = mtime*6 + stime/10;
+    srotation = stime*6;
+
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
